4aebe198b488c98c4b10888aaaba0237f0dd214a
Andre <andre@WEIs-MacBook.local>
2016-07-16 22:57:20 -0700

    add mapreduce version of countAndSay

--- nodeDemo/countAndSay.js
+++ nodeDemo/countAndSay.js
@@ -1,42 +1,42 @@
 var countAndSay = function(n) {
-	//n is divided by 10, and every level of 10 has the number of the current level
-    //solution: using string[]
-    //iterate start from 1 to n-1, stores visted value, if current value equals visited value, increase num, else print visited value
-    
-    //split values into array
-    //loop and store visited value,
-    //if encountered before, num++,
-    //otherwise, output visited value
-    if (!n || !Number.isInteger(n) || n < 0) return null; //return null if n is exceptional
+    //split into array.
+    //loop each item, store current value 
+    //and push into stack
+    //if current item same as peak, continue push
+    //otherwise, pop whole stack out with length value
 
-    if (n < 10) return('1'+n);  //a single number
+    var input = (''+n).split('');
+    var output = [];
+    var temp = [];
 
-    var array = (n+'').split('');
-    var visitedVal = array[0];
-    var num = 1;
-    var output = new Array();
-    for(var i = 1; i < array.length; i++) {
-    	var val = array[i];
-    	if (visitedVal === val) {
-    		num++;
-    	}
-    	else {
-    		output.push(num.toString());
-    		output.push(visitedVal);
-    		num = 1;
-    		visitedVal = val;
-    	}
-
-		if (i === array.length-1) {
-			output.push(num.toString());
-			output.push(visitedVal);
-		}  	
+    //
+    for (var val of input) {
+        //when value same as temp peak, or temp is empty
+        if (temp.length === 0 || val === temp[0]) { 
+            temp.push(val); //put current value into stack
+        }
+        else {  //when curent value not the same as peak
+            //1. got length of temp, and insert into output
+            output.push(temp.length.toString());
+            //2. insert temp peak value into output
+            output.push(temp[0]);
+            //3. pop out all values from temp
+            temp = [];
+            //4. insert current value to temp
+            temp.push(val); 
+        }
     }
-    
+
+    //don't forget the remains in the temp
+    if (temp.length > 0) {
+        output.push(temp.length.toString());
+        output.push(temp[0]);
+        temp = [];
+    }
+
     return output.join('');
-    //alter: using int only
 };
 
-var n = 55331111;
+var n = 'abbccddn';
 console.log(n);
 console.log(countAndSay(n));
