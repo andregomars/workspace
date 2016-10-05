ca52a69021d4a67ad67dbd9a247e748a44d9cba5
Andre.W.Shen (tech.usca11.Newegg) 22513 <as13@buyabs.corp>
2016-10-04 17:53:51 -0700

    add hangfire

--- netCoreDemo/WebApp/Startup.cs
+++ netCoreDemo/WebApp/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Hangfire;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApp
 {
@@ -16,11 +18,16 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHangfire(x => x.UseSqlServerStorage("Server=localhost;Database=ioc;User Id=iocdbo;Password=iocdbo123!@#;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //GlobalConfiguration.Configuration.UseSqlServerStorage("<connection string or its name>");
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+/*
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
@@ -32,6 +39,7 @@ namespace WebApp
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+*/
         }
     }
 }
