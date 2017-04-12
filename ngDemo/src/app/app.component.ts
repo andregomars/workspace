9f56ea286b8634e90e6b9c9d6543c4505f658ec6
andregomars <andregomars@gmail.com>
2017-04-11 22:21:36 -0700

    update ngdemo

--- ngDemo/src/app/app.component.ts
+++ ngDemo/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as Rx from 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/operator/map';
+import { Http, Response } from '@angular/http';
+
+const URL_VehicleIdentities = "http://www.mocky.io/v2/58e926392a000014022dbbb4";
 
 @Component({
   selector: 'app-root',
@@ -14,14 +16,33 @@ export class AppComponent {
   private data: Array<Person> = [{name:'andre', age:55},{name:'carol', age:33}];
   dataFrom$: Observable<any>;
   dataOf$: Observable<any>;
+  restOf$: Observable<any>;
+  restFlatMap = [];
+  vehicles = [];
 
-  constructor() {
+  constructor(
+    private http: Http
+  ) {
   }
 
   ngOnInit(): void {
       this.dataFrom$ = Rx.Observable.from(this.data);
       this.dataOf$ = Rx.Observable.of(this.data);
-        // .map(v => v.name);
+      // this.restOf$ = this.http.get(URL_VehicleIdentities)
+      //   .map(res => res.json()
+      //     .map(v => v.fid)
+      //     .filter((el, i, arr) => arr.indexOf(el) === i)
+      //   )
+      //   .do(data => console.log(JSON.stringify(data)));
+      var sub = this.http.get(URL_VehicleIdentities)
+        .flatMap(data => data.json())
+        .publishReplay(1)
+        .refCount();
+
+      sub.subscribe(data => this.restFlatMap.push(data));
+      sub.subscribe(data => console.log(data));
+
+
   }
 }
 
