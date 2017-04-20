986a8b796d72ef6b8f75eca728c6d068e0aea302
andre shen <andregomars@gmail.com>
2017-04-20 14:30:59 -0700

    init webapi through dotnet cli

---
+++ efCoreDemo/Repositories/ITodoRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using TodoApi.Models;
+
+namespace TodoApi.Repositories
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(long key);
+        void Remove(long key);
+        void Update(TodoItem item);
+    }
+}
