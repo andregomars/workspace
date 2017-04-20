e3923ccbc3719520fb829006a605a3d2d419356c
andre shen <andregomars@gmail.com>
2017-04-20 15:56:14 -0700

    add controllers

--- coreApiDemo/Startup.cs
+++ coreApiDemo/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+using TodoApi.Models;
+using TodoApi.Repositories;
 
 namespace coreApiDemo
 {
@@ -27,8 +31,10 @@ namespace coreApiDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
             // Add framework services.
             services.AddMvc();
+            services.AddScoped<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
