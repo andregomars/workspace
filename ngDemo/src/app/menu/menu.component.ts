8cd674efcce111f21fbe507330cf30bcd9df618f
andre shen <andregomars@gmail.com>
2017-05-26 17:47:18 -0700

    updat menu style

--- ngDemo/src/app/menu/menu.component.ts
+++ ngDemo/src/app/menu/menu.component.ts
@@ -1,20 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MenuItem } from 'primeng/primeng';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MenuComponent implements OnInit {
 
   vehicleItems: any;
   fleetItem: any;
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init');
-      this.fleetItem = [{
+    this.initDemoMenu();
+  }
+
+  private initDemoMenu(): void {
+    this.items = [
+            {
+                label: 'File',
+                icon: 'fa-file-o',
+                items: [{
+                        label: 'New', 
+                        icon: 'fa-plus',
+                        items: [
+                            {label: 'Project'},
+                            {label: 'Other'},
+                        ]
+                    },
+                    {label: 'Open'},
+                    {label: 'Quit'}
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'fa-edit',
+                items: [
+                    {label: 'Undo', icon: 'fa-mail-forward'},
+                    {label: 'Redo', icon: 'fa-mail-reply'}
+                ]
+            },
+            {
+                label: 'Help',
+                icon: 'fa-question',
+                items: [
+                    {
+                        label: 'Contents'
+                    },
+                    {
+                        label: 'Search', 
+                        icon: 'fa-search', 
+                        items: [
+                            {
+                                label: 'Text', 
+                                items: [
+                                    {
+                                        label: 'Workspace'
+                                    }
+                                ]
+                            },
+                            {
+                                label: 'File'
+                            }
+                    ]}
+                ]
+            },
+            {
+                label: 'Actions',
+                icon: 'fa-gear',
+                items: [
+                    {
+                        label: 'Edit',
+                        icon: 'fa-refresh',
+                        items: [
+                            {label: 'Save', icon: 'fa-save'},
+                            {label: 'Update', icon: 'fa-save'},
+                        ]
+                    },
+                    {
+                        label: 'Other',
+                        icon: 'fa-phone',
+                        items: [
+                            {label: 'Delete', icon: 'fa-minus'}
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Quit', icon: 'fa-minus'
+            }
+        ];
+  }
+  
+  private initHamsMenu(): void {
+    this.fleetItem = [{
             label: 'LBT',
             routerLink: ['/fleet', 'LBT'],
             items: [
@@ -42,7 +125,4 @@ export class MenuComponent implements OnInit {
         }
     ];
   }
-
-
-  
 }
