1c906458cecf8f5bb41532234a8174d7ef4e347a
andregomars <andregomars@gmail.com>
2017-10-20 00:25:12 -0700

    add mongodb test

---
+++ pythonDemo/bottleDemo/rest.py
@@ -0,0 +1,37 @@
+from bottle import run, route, request, error
+
+@error(404)
+def error404(error):
+  return "page not found!"
+
+@error(405)
+def error405(error):
+  return "method is not allowed!!"
+
+
+
+@route('/')
+def index():
+  return "hi there do we!"
+
+@route('/login')
+def login():
+  return "you logged in"
+
+@route('/vendors/<vid>')
+def login(vid):
+  return "vendor " + vid
+
+@route('/vendors')
+def loginQuery():
+  vid = request.query.vid
+  # return "vendor " + vid
+  return {"vid": vid, "name": "ingram"}
+
+@route('/addnewvendor', method='POST')
+def addVendor():
+  return "add new vendor" 
+
+
+
+# run(debug=False, reloader=True)
