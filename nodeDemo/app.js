fb5b0d3cd8a4308e3df9cc9aadb9305e9d7d3f8f
andregomars <andregomars@gmail.com>
2017-10-22 22:49:17 -0700

    test clivas

--- nodeDemo/app.js
+++ nodeDemo/app.js
@@ -1,9 +1,23 @@
 var assert = require('assert');
+var clivas = require('clivas');
 
-
+/*
 function reverseWords(words) {
 	return words.split(' ').reverse().join(' ');
 }
 
 assert.equal(reverseWords('andre go mars')
 	, 'mars go andre');
+*/
+
+
+var clivas = require('clivas');
+
+var frame = 0;
+
+setInterval(function() {
+	clivas.clear(); // clears the canvas
+	clivas.line('hello world (#frame '+frame+')');
+	clivas.line('{red:also} {green:colors}!');
+	frame++;
+}, 200);
