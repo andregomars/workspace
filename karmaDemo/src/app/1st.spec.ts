34b651d08b3978d6f1cdb0c1174cc3276a427bfd
andregomars <andregomars@gmail.com>
2018-01-22 22:10:50 -0800

    add karma test demo

---
+++ karmaDemo/src/app/1st.spec.ts
@@ -0,0 +1,3 @@
+describe('1st tests', () => {
+    it('ture is ture', () => expect(true).toBe(true));
+});
---
+++ karmaDemo/src/app/banner/banner.component.spec.ts
@@ -0,0 +1,40 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BannerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should return new value', () => {
+    component.title = 'world';
+    fixture.detectChanges();
+    expect(el.textContent).toContain('world');
+  });
+
+  it('should return hello', () => {
+    expect(el.textContent).toContain('hello');
+  });
+});
