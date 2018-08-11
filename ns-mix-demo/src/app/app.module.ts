4ae48fc06265573c04d75b5f725d2d96de932e1b
Andre <andre@LIJUANs-MacBook.local>
2018-08-11 15:29:37 -0700

    add player module for html

--- ns-mix-demo/src/app/app.module.ts
+++ ns-mix-demo/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BarcelonaModule } from './barcelona/barcelona.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BarcelonaModule,
     AppRoutingModule
   ],
   providers: [],
