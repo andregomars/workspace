03578b512984ebc98831fb87cd1faf8cde1f7b5d
andre <andregomars@gmail.com>
2018-09-05 16:07:25 -0700

    adjust fb auth

--- ng-fire-demo/src/app/view/dashboard/dashboard.component.ts
+++ ng-fire-demo/src/app/view/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from 'src/app/core/auth.service';
 import { User } from 'src/app/model/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,17 +11,40 @@ import { User } from 'src/app/model/user';
 })
 export class DashboardComponent implements OnInit {
   user: User;
+  user$: Observable<User>;
 
   constructor(
     private authService: AuthService
   ) { }
 
   ngOnInit() {
-    this.authService.user.subscribe(data => this.user = data);
+    this.user$ = this.authService.user;
   }
 
   signOut() {
     this.authService.signOut();
   }
 
+  isCapableOf(user: User, cap: Capability) {
+    let isCapable = false;
+    switch (cap) {
+      case Capability.Edit:
+        isCapable = this.authService.canEdit(user);
+        break;
+      case Capability.View:
+        isCapable = this.authService.canRead(user);
+        break;
+      default:
+        isCapable = this.authService.canRead(user);
+        break;
+    }
+
+    return isCapable;
+  }
+
+}
+
+export enum Capability {
+  View = 'View',
+  Edit = 'Edit'
 }
