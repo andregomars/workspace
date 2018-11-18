223ee1e79d287b988a3180e65caf58a8d32fcbb5
andregomars <andregomars@gmail.com>
2018-11-18 10:27:53 -0800

    print out remote ip address

--- node-tcp/app.js
+++ node-tcp/app.js
@@ -3,14 +3,20 @@ const net = require('net');
 const shortid = require('shortid');
 
 const tcpServer = net.createServer();
-tcpServer.on('connection', function(sock) {
+tcpServer.on('connection', function(socket) {
     console.log('start connection');
+    console.log(socket.remoteAddress);
 
-    sock.on('data', function(data) {
-        fs.writeFile(`./output/data-${shortid.generate()}.bin`, data, function(error) {
-            console.log('data written')
+    try {
+        socket.on('data', function(data) {
+            fs.writeFile(`./output/data-${shortid.generate()}.bin`, data, function(error) {
+                console.log('data written')
+            });
         });
-    });
+    } catch(error) {
+        console.log(error);
+    }
+
 });
 
 tcpServer.listen(5000);
