e96ccdb0744e740883398642824a4e43515f1e94
andre <andregomars@gmail.com>
2020-05-19 17:06:01 -0700

    add puttest

---
+++ puptest/src/main.ts
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer';
+const email = 'andregomars.aws02@gmail.com';
+const password = 'andregomars.aws02@gmail.co';
+const screenshot = 'loggedin.png';
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setViewport({ width: 1920, height: 1080});
+
+    await page.goto('https://www.papermart.com/login', { waitUntil: 'networkidle0' });
+    await page.type('#mat-input-2', email); // email input
+    await page.type('#mat-input-3', password); // password input
+    await page.click('[type=submit]'); // sign in button
+    // const loginError = await page.$('#mat-error-0');
+    const loginError = await page.waitForSelector('#mat-error-0');
+    // if (loginError) {
+    //     console.error('login failed');
+    // } else {
+    //     await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    //     await page.screenshot({ path: screenshot });
+    //     console.log(`screenshot saved as ${screenshot}`)
+    // }
+
+    await browser.close();
+})()
